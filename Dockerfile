ab9593f6e2cbef1285023f0d2904145473e2af87
yazanash <yazan.ash.doonaas@gmail.com>
2025-09-27 13:25:43 +0300

    add docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18.18.2-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+'use client'
+import React, { useEffect } from 'react';
 import Hero from './Hero/Hero';
 import BackgroundHero from './Hero/BackgroundHero';
 import Actvities from './Activities/Actvities';
 import About from './about/About';
 import Blog from './Blog/Blog';
+// import Aos from 'aos';
+// import 'aos/dist/aos.css';
 const Home = () => {
+
+  // useEffect(()=>{
+  //   const initAos = async ()=>{
+  //     await import('aos');
+  //     Aos.init({
+  //       duration: 1000,
+  //       easing:'ease',
+  //       once:true,
+  //       anchorPlacement:'top-bottom'
+
+  //     })
+  //     initAos();
+  //   }
+  // },[])
+
   return (
     <div className='overflow-hidden'>
       <BackgroundHero/>
